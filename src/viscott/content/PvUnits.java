b04b0f3e2b00f0999b6acb7af27414cb284299de
siede2010 <63005632+siede2010@users.noreply.github.com>
2023-01-21 16:37:42 +0100

    Another Core Unit...

--- src/viscott/content/PvUnits.java
+++ src/viscott/content/PvUnits.java
@@ -12,14 +12,14 @@ import viscott.utilitys.PvUtil;
 
 public class PvUnits {
     public static UnitType
-        micro
+        micro,infrared
             ;
     public static void load()
     {
         micro = new UnitType("micro")
         {{
             localizedName = "Micro";
-            constructor = EntityMapping.map("flare");
+            constructor = EntityMapping.map("alpha");
             health = 175;
             armor = 0;
             flying = true;
@@ -46,11 +46,49 @@ public class PvUnits {
                         homingDelay = 1;
                         homingRange = 8*17f;
                         lifetime = PvUtil.GetRange(this.speed,17);
-                        lightColor = backColor = Pal.heal;
+                        lightColor = backColor = Pal.engine;
                         this.recoil = 0.1f;
                         despawnShake = hitShake = 0.5f;
                     }};
                 }}
+            );
+        }};
+        infrared = new UnitType("infrared")
+        {{
+            localizedName = "Infrared";
+            constructor = EntityMapping.map("beta");
+            health = 190;
+            armor = 0;
+            flying = true;
+            buildSpeed = 0.9f;
+            mineTier = 2;
+            mineSpeed = 6.5f;
+            itemCapacity = 65;
+            speed = 22.8f / 7.5f;
+            drag = 0.02f;
+            range = 18*8;
+            weapons.add(
+                    new Weapon("")
+                    {{
+                        x = 0;
+                        y = 5;
+                        reload = 60f/2f;
+                        rotate = true;
+                        rotationLimit = 30;
+                        shootStatus = PvStatusEffects.expent;
+                        shootStatusDuration = 120;
+                        bullet = new LaserBoltBulletType(5,6)
+                        {{
+                            homingPower = 0.01f;
+                            trailLength = 20;
+                            trailWidth = 2;
+                            homingDelay = 1;
+                            homingRange = 8*17f;
+                            lifetime = PvUtil.GetRange(this.speed,17);
+                            trailColor = lightColor = backColor = Pal.engine;
+                            despawnShake = hitShake = 0.5f;
+                        }};
+                    }}
             );
         }};
     }
