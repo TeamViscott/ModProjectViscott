7a025e5321462098702d7ff198b61e2bd9d3da78
ManuWar73 <manuelsf2009@gmail.ccom>
2023-01-21 20:50:03 +0100

    Rainmaker turret changes 2

--- src/viscott/content/shootpatterns/CyclicPatternRainmaker.java
+++ src/viscott/content/shootpatterns/CyclicPatternRainmaker.java
@@ -15,7 +15,7 @@ public class CyclicPatternRainmaker extends ShootSpread {
     @Override
     public void shoot(int totalShots, BulletHandler handler){
         ShotsPerCycle = 5;
-        spread = 4;
+        spread = 25f / ShotsPerCycle * Cycle;
         MaxCycleIteration = 9;
         shots = Cycle * ShotsPerCycle;
         for(int i = 0; i < shots; i++){
