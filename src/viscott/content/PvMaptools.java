c5e7e30f72242a6569258dbb8b911d1bea8cecae
ManuWar73 <manuelsf2009@gmail.ccom>
2023-02-11 19:48:28 +0100

    heat and power sources

--- src/viscott/content/PvMaptools.java
+++ src/viscott/content/PvMaptools.java
@@ -8,11 +8,16 @@ import mindustry.type.UnitType;
 import mindustry.world.Block;
 import mindustry.world.blocks.distribution.Conveyor;
 import mindustry.world.blocks.distribution.Router;
+import mindustry.world.blocks.heat.HeatProducer;
 import mindustry.world.blocks.liquid.Conduit;
 import mindustry.world.blocks.liquid.LiquidRouter;
 import mindustry.world.blocks.sandbox.ItemSource;
 import mindustry.world.blocks.sandbox.LiquidSource;
+import mindustry.world.blocks.sandbox.PowerSource;
 import mindustry.world.blocks.storage.CoreBlock;
+import mindustry.world.draw.DrawDefault;
+import mindustry.world.draw.DrawHeatOutput;
+import mindustry.world.draw.DrawMulti;
 import mindustry.world.meta.BuildVisibility;
 
 import javax.xml.transform.Source;
@@ -22,7 +27,7 @@ import static mindustry.type.ItemStack.with;
 public class PvMaptools {
     public static Block
     basicsource, normalsource, advancedsource, ultrasource, ultrarouter, ultraliquidrouter, omegarouter, omegaliquidrouter, ultraconveyor,
-    ultraconduit, basicliquidsource, advancedliquidsource, ultraliquidsource, normalliquidsource, coreIntrusion;
+    ultraconduit, basicliquidsource, advancedliquidsource, ultraliquidsource, normalliquidsource, coreIntrusion, ultraheatsource, ultrapowersource;
     public static void load()
     {
         basicsource = new ItemSource("basicsource")
@@ -31,6 +36,7 @@ public class PvMaptools {
             localizedName = "Basic Source";
             itemsPerSecond = 2;
             health = 600;
+            buildVisibility = BuildVisibility.sandboxOnly;
 
         }};
         normalsource = new ItemSource("normalsource")
@@ -39,6 +45,7 @@ public class PvMaptools {
             localizedName = "Medium Source";
             itemsPerSecond = 5;
             health = 1000;
+            buildVisibility = BuildVisibility.sandboxOnly;
 
         }};
         advancedsource = new ItemSource("advancedsource")
@@ -47,6 +54,7 @@ public class PvMaptools {
             localizedName = "Advanced Source";
             itemsPerSecond = 7;
             health = 4000;
+            buildVisibility = BuildVisibility.sandboxOnly;
 
         }};
         ultrasource = new ItemSource("ultrasource")
@@ -55,6 +63,7 @@ public class PvMaptools {
             localizedName = "Indestructible Source";
             itemsPerSecond = 10;
             health = 2147483647;
+            buildVisibility = BuildVisibility.sandboxOnly;
 
         }};
         ultraconveyor = new Conveyor("ultraconveyor")
@@ -80,6 +89,7 @@ public class PvMaptools {
             size = 1;
             localizedName = "Basic Liquid Source";
             health = 600;
+            buildVisibility = BuildVisibility.sandboxOnly;
 
         }};
         normalliquidsource = new LiquidSource("normalliquidsource")
@@ -87,6 +97,7 @@ public class PvMaptools {
             size = 2;
             localizedName = "Medium Liquid Source";
             health = 1000;
+            buildVisibility = BuildVisibility.sandboxOnly;
 
         }};
         advancedliquidsource = new LiquidSource("advancedliquidsource")
@@ -94,6 +105,7 @@ public class PvMaptools {
             size = 3;
             localizedName = "Advanced Liquid Source";
             health = 4000;
+            buildVisibility = BuildVisibility.sandboxOnly;
 
         }};
         ultraliquidsource = new LiquidSource("ultraliquidsource")
@@ -101,6 +113,7 @@ public class PvMaptools {
             size = 5;
             localizedName = "Indestructible Liquid Source";
             health = 2147483647;
+            buildVisibility = BuildVisibility.sandboxOnly;
 
         }};
         ultraliquidrouter = new LiquidRouter("ultraliquidrouter")
@@ -108,6 +121,7 @@ public class PvMaptools {
             size = 1;
             localizedName = "Indestructible Liquid Router";
             health = 2147483647;
+            buildVisibility = BuildVisibility.sandboxOnly;
 
         }};
         ultrarouter = new Router("ultrarouter")
@@ -134,6 +148,7 @@ public class PvMaptools {
                 size = 2;
                 localizedName = "Indestructible Liquid Distributor";
                 health = 2147483647;
+                buildVisibility = BuildVisibility.sandboxOnly;
             }};
         coreIntrusion = new CoreBlock("core-intrusion")
         {{
@@ -141,9 +156,30 @@ public class PvMaptools {
             alwaysUnlocked = true;
             unitType = UnitTypes.emanate; //todo emanate like unit for invasion mode
             health = 5350;
-            size = 5;
+            size = 6;
             unitCapModifier = 100;
             itemCapacity = 10000;
+            buildVisibility = BuildVisibility.sandboxOnly;
+        }};
+        ultraheatsource = new HeatProducer("ultraheatsource")
+        {{
+            size = 1;
+            localizedName = "Indestructible Heat Source";
+            health = 2147483647;
+            drawer = new DrawMulti(new DrawDefault(), new DrawHeatOutput());
+            heatOutput = 1000;
+            buildVisibility = BuildVisibility.sandboxOnly;
+
+        }};
+        ultrapowersource = new PowerSource("ultrapowersource")
+        {{
+            size = 1;
+            localizedName = "Indestructible Power Source";
+            health = 2147483647;
+            powerProduction = 10000000;
+            laserRange = 1;
+            buildVisibility = BuildVisibility.sandboxOnly;
+
         }};
         }
 }
