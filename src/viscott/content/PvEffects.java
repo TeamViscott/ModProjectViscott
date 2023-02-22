04db4f27a27e3323f69ab0f1a12c21ee5dace999
siede2010 <63005632+siede2010@users.noreply.github.com>
2023-02-22 20:14:07 +0100

    Fragment and Nueromass Conveyor

--- src/viscott/content/PvEffects.java
+++ src/viscott/content/PvEffects.java
@@ -1,6 +1,8 @@
 package viscott.content;
 
+import arc.math.Mathf;
 import mindustry.entities.Effect;
+import mindustry.graphics.Drawf;
 import mindustry.graphics.Pal;
 
 import static arc.graphics.g2d.Draw.rect;
@@ -9,7 +11,7 @@ import static arc.graphics.g2d.Lines.*;
 
 public class PvEffects {
     public static Effect
-        slowEnergeticEffect, particleDeath
+        slowEnergeticEffect, particleDeath1,particleDeath2,particleDeath3,railFrag
             ;
     public static void load()
     {
@@ -19,11 +21,35 @@ public class PvEffects {
             stroke(e.fout()*4);
             circle(e.x,e.y,e.fin()*8.3f*8);
         });
-        particleDeath = new Effect(100, e -> {
+        particleDeath1 = new Effect(100, e -> {
             color(Pal.sap);
             alpha(e.fout());
             stroke(e.fout()*4);
             circle(e.x,e.y,e.fin()*8);
+        });
+        particleDeath2 = new Effect(100, e -> {
+            color(Pal.sap);
+            alpha(e.fout());
+            stroke(e.fout()*8);
+            circle(e.x,e.y,e.fin()*16);
+            circle(e.x,e.y,e.fin()*8);
+        });
+        particleDeath3 = new Effect(100, e -> {
+            color(Pal.sap);
+            alpha(e.fout());
+            stroke(e.fout()*8);
+            circle(e.x,e.y,e.fin()*24);
+            circle(e.x,e.y,e.fin()*16);
+            circle(e.x,e.y,e.fin()*8);
+        });
+        railFrag = new Effect(32f, e -> {
+            color(Pal.sap);
+
+            for(int i : Mathf.signs){
+                Drawf.tri(e.x, e.y, 20f * e.fout(), 30f, e.rotation + 90 + 90f * i);
+            }
+
+            Drawf.light(e.x, e.y, 60f * e.fout(), Pal.orangeSpark, 0.5f);
         });
     }
 }
