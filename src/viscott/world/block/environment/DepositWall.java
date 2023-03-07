b7d8ba411f6d499bf57bd90d5a2c6eb7dde86032
siede2010 <63005632+siede2010@users.noreply.github.com>
2023-03-07 16:33:46 +0100

    Updated deposits

--- src/viscott/world/block/environment/DepositWall.java
+++ src/viscott/world/block/environment/DepositWall.java
@@ -2,18 +2,22 @@ package viscott.world.block.environment;
 
 import arc.Core;
 import arc.graphics.g2d.Draw;
+import arc.graphics.g2d.TextureRegion;
 import arc.math.Mathf;
 import arc.math.geom.Point2;
 import mindustry.world.Tile;
 import mindustry.world.blocks.environment.Floor;
 import mindustry.world.blocks.environment.StaticWall;
+import mindustry.world.blocks.environment.TallBlock;
 import viscott.world.block.drill.Grinder;
 
 import static mindustry.Vars.world;
 
-public class DepositWall extends StaticWall {
+public class DepositWall extends TallBlock {
     public float tier = 1;
     public float hardness = 0;
+
+    public TextureRegion large;
 
     public DepositWall(String name)
     {
@@ -22,6 +26,8 @@ public class DepositWall extends StaticWall {
 
     @Override
     public void drawBase(Tile tile){
+        if (large == null)
+            large = Core.atlas.find(name + "-large");
         int rx = tile.x/2*2, ry = tile.y/2*2;
         if (canLarge(rx,ry) && Mathf.randomSeed(Point2.pack(rx,ry)) < 0.5) {
             if (tile.x / 2 * 2 == tile.x && tile.y / 2 * 2 == tile.y)
