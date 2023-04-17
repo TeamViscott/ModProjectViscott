e42d124fcaa5dbaba3c8e63306eb47e9b6ba10dc
siede2010 <63005632+siede2010@users.noreply.github.com>
2023-04-17 16:15:50 +0200

    vdoble

--- src/viscott/content/PvUnits.java
+++ src/viscott/content/PvUnits.java
@@ -3,6 +3,7 @@ package viscott.content;
 import arc.graphics.Color;
 import arc.struct.Seq;
 import mindustry.content.Fx;
+import mindustry.content.Items;
 import mindustry.content.StatusEffects;
 import mindustry.entities.bullet.*;
 import mindustry.entities.effect.ExplosionEffect;
@@ -12,12 +13,14 @@ import mindustry.entities.part.HaloPart;
 import mindustry.entities.part.RegionPart;
 import mindustry.entities.part.ShapePart;
 import mindustry.entities.pattern.ShootHelix;
+import mindustry.entities.pattern.ShootSpread;
 import mindustry.gen.EntityMapping;
 import mindustry.gen.Sounds;
 import mindustry.graphics.Layer;
 import mindustry.graphics.Pal;
 import mindustry.type.UnitType;
 import mindustry.type.Weapon;
+import mindustry.type.ammo.ItemAmmoType;
 import viscott.abilitys.EnemyStatusFieldAbility;
 import viscott.types.NullisUnitType;
 import viscott.types.PvUnitType;
@@ -34,7 +37,10 @@ public class PvUnits {
             /*Storage Con Path*/pocket,container,
 
         /*Extra Paths : */
-        routerTank, routerBastion
+        routerTank, routerBastion,
+
+                /*BOSSES*/
+                    vdoble
                 ;
     public static void load()
     {
@@ -42,6 +48,7 @@ public class PvUnits {
         loadCorePath();
         loadStoragePath();
         loadExtra();
+        loadBosses();
     }
     public static void loadCorePath()
     {
@@ -913,6 +920,139 @@ public class PvUnits {
                     new UnitEngine(-10,-10,6,180+45),
                     new UnitEngine(10,-10,6,270+45)
             );
+        }};
+    }
+    public static void loadBosses()
+    {
+        vdoble = new PvUnitType("vdoble")
+        {{
+            localizedName = "Vdoble";
+            constructor = EntityMapping.map("toxopid");
+            drag = 0.1f;
+            speed = 3.2f/7.5f;
+            hitSize = 26f;
+            health = 110000;
+            armor = 20f;
+            lightRadius = 140f;
+
+            rotateSpeed = 1.9f;
+            drownTimeMultiplier = 3f;
+
+            legCount = 12;
+            legMoveSpace = 0.8f;
+            legPairOffset = 3;
+            legLength = 90f;
+            legExtension = -20;
+            legBaseOffset = 8f;
+            stepShake = 1f;
+            legLengthScl = 0.93f;
+            rippleScale = 3f;
+            legSpeed = 0.19f;
+            ammoType = new ItemAmmoType(Items.graphite, 8);
+
+            deathExplosionEffect = PvEffects.nullisDeath.get(3);
+            legSplashDamage = 80;
+            legSplashRange = 60;
+
+            hovering = true;
+            shadowElevation = 0.95f;
+            groundLayer = Layer.legUnit;
+
+            weapons.add(
+                    new Weapon("large-purple-mount"){{
+                        y = -5f;
+                        x = 11f;
+                        shootY = 7f;
+                        reload = 30;
+                        shake = 4f;
+                        rotateSpeed = 2f;
+                        ejectEffect = Fx.casing1;
+                        shootSound = Sounds.shootBig;
+                        rotate = true;
+                        shadow = 12f;
+                        recoil = 3f;
+
+                        shoot = new ShootSpread(2, 17f);
+
+                        bullet = new ShrapnelBulletType(){{
+                            length = 90f;
+                            damage = 110f;
+                            width = 25f;
+                            serrationLenScl = 7f;
+                            serrationSpaceOffset = 60f;
+                            serrationFadeOffset = 0f;
+                            serrations = 10;
+                            serrationWidth = 6f;
+                            fromColor = Pal.sapBullet;
+                            toColor = Pal.sapBulletBack;
+                            shootEffect = smokeEffect = Fx.sparkShoot;
+                        }};
+                    }});
+
+            weapons.add(new Weapon("toxopid-cannon"){{
+                y = -14f;
+                x = 0f;
+                shootY = 22f;
+                mirror = false;
+                reload = 210;
+                shake = 10f;
+                recoil = 10f;
+                rotateSpeed = 1f;
+                ejectEffect = Fx.casing3;
+                shootSound = Sounds.artillery;
+                rotate = true;
+                shadow = 30f;
+
+                rotationLimit = 80f;
+
+                bullet = new ArtilleryBulletType(3f, 50){{
+                    hitEffect = Fx.sapExplosion;
+                    knockback = 0.8f;
+                    lifetime = 80f;
+                    width = height = 25f;
+                    collidesTiles = collides = true;
+                    ammoMultiplier = 4f;
+                    splashDamageRadius = 80f;
+                    splashDamage = 75f;
+                    backColor = Pal.sapBulletBack;
+                    frontColor = lightningColor = Pal.sapBullet;
+                    lightning = 5;
+                    lightningLength = 20;
+                    smokeEffect = Fx.shootBigSmoke2;
+                    hitShake = 10f;
+                    lightRadius = 40f;
+                    lightColor = Pal.sap;
+                    lightOpacity = 0.6f;
+
+                    status = StatusEffects.sapped;
+                    statusDuration = 60f * 10;
+
+                    fragLifeMin = 0.3f;
+                    fragBullets = 9;
+
+                    fragBullet = new ArtilleryBulletType(2.3f, 30){{
+                        hitEffect = Fx.sapExplosion;
+                        knockback = 0.8f;
+                        lifetime = 90f;
+                        width = height = 20f;
+                        collidesTiles = false;
+                        splashDamageRadius = 70f;
+                        splashDamage = 40f;
+                        backColor = Pal.sapBulletBack;
+                        frontColor = lightningColor = Pal.sapBullet;
+                        lightning = 2;
+                        lightningLength = 5;
+                        smokeEffect = Fx.shootBigSmoke2;
+                        hitShake = 5f;
+                        lightRadius = 30f;
+                        lightColor = Pal.sap;
+                        lightOpacity = 0.5f;
+
+                        status = StatusEffects.sapped;
+                        statusDuration = 60f * 10;
+                    }};
+                }};
+            }});
         }};
     }
 }
